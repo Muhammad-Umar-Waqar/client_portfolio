d229422696c6a24721a1b4a92cb61f119e9f10da
Muhammad Umar Waqar <mohammthegreat@gmail.com>
2025-04-06 06:29:44 +0500

    Ading fixed!

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,16 +5,7 @@ export default function Footer() {
     return (
         <footer className="bg-white border-y">
             <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
-                <div className="md:flex md:justify-between">
-                    <div className="mb-6 md:mb-0">
-                        <Link to="/" className="flex items-center">
-                            <img
-                                src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
-                                className="mr-3 h-16"
-                                alt="Logo"
-                            />
-                        </Link>
-                    </div>
+
                     <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
                         <div>
                             <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase">Resources</h2>
@@ -67,17 +58,17 @@ export default function Footer() {
                             </ul>
                         </div>
                     </div>
-                </div>
+             
                 <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
                 <div className="sm:flex sm:items-center sm:justify-between">
                     <span className="text-sm text-gray-500 sm:text-center">
-                        © 2023
+                        © 2025 
                         <a href="https://www.linkedin.com/in/muhammad-umar-4266a01b2/" className="hover:underline">
-                            Muhammad Umar Waqar
+                        {"\u00A0"}Mohsina Shinwari
                         </a>
                         . All Rights Reserved.
                     </span>
-                    <div className="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
+                    <div className="flex mt-4 space-x-5 justify-center sm:mt-0">
                         <Link to="#" className="text-gray-500 hover:text-gray-900">
                             <svg
                                 className="w-4 h-4"
