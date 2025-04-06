01dc21fbb1ef07dd013e3bbd687c4fc156c9b52b
Muhammad Umar Waqar <mohammthegreat@gmail.com>
2025-04-06 05:58:39 +0500

    Adding Completed

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 import axios from "axios";
 import { motion } from "framer-motion";
 import { useState, useEffect } from "react";
